981887433d6bf41463acb07047132600a6d27303
bycait27 <caitlinba27@gmail.com>
2026-06-26 19:16:18 -0500

    Add Hello World program implementation for Sprint 1

---
+++ src/main/java/com/caitlinash/poketrainermanager/App.java
@@ -0,0 +1,11 @@
+package com.caitlinash.poketrainermanager;
+
+/* 
+* Hello World program: prints "Hello World" to the terminal
+*/
+
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
